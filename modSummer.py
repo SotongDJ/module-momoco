fc6a3b9edca6b1e9a7e9038200c6406327012b87
David Soh <sotong0221@gmail.com>
2017-12-10 18:07:39 +0800

    finish stage two

--- modSummer.py
+++ modSummer.py
@@ -4,8 +4,7 @@ from core import modVariables
 def sumer(usrdir,dicto):
     print('modSummer.sumer: '+usrdir)
     libra = modDatabase.opendb(usrdir)
-    modDatabase.getKaratio(usrdir)
-    karatio = modDatabase.openKaratio(usrdir)
+    karat = modDatabase.openKaratio(usrdir)
     libra = modDatabase.opendb(usrdir)
     rawdb = libra.get('raw',{})
     keydb = libra.get('key',{})
@@ -13,8 +12,8 @@ def sumer(usrdir,dicto):
     dtempo = dicto.get('dtempo','')
     utempo = dicto.get('utempo','')
     takas = dicto.get('takas','')
-    kekad = dicto.get('kekas','')
-    keywo = dicto.get('keywo','')
+    kekas = dicto.get('kekas','')
+    keywo = dicto.get(kekas,'')
 
     tiset = modDatabase.timra(usrdir, dtempo=dtempo, utempo=utempo)
 
@@ -23,9 +22,9 @@ def sumer(usrdir,dicto):
     stagun = []
     for rekod in tiset:
         sorse = rawdb.get(rekod,{})
-        for keywo in list(sorse.keys()):
+        for keysa in list(sorse.keys()):
             if dicto.get(keywo,'') != '':
-                if sorse.get(keywo,'') != dicto.get(keywo,''):
+                if sorse.get(keysa,'') != dicto.get(keywo,''):
                     modee = False
         if modee:
             stagun.append(rekod)
@@ -34,9 +33,12 @@ def sumer(usrdir,dicto):
     stados = {}
     for rekod in stagun:
         sorse = rawdb.get(rekod,{})
+        print(sorse.get(kekas,'')+' - '+keywo)
         if sorse.get(kekas,'') == keywo:
             taket = sorse.get(takas,'')
-            upvalu = stados.get(taket,0.0)
+            print('kekas:'+kekas+', value:'+sorse.get(kekas,''))
+            print('takas:'+takas+', taket:'+taket)
+            upvalu = stados.get(taket,0.0) + float(sorse.get('tpric',''))
             stados.update({ taket : upvalu })
 
-    return {'stagun':staguno, 'stados':stados}
+    return {'stagun':stagun, 'stados':stados}
