2d1fdbfab3eee4df6f3b3b5f8b53abfffe5cfd6d
David Soh <sotong0221@gmail.com>
2017-12-10 17:28:09 +0800

    start stage two (stil working on develop)

--- modSummer.py
+++ modSummer.py
@@ -4,17 +4,23 @@ from core import modVariables
 def sumer(usrdir,dicto):
     print('modSummer.sumer: '+usrdir)
     libra = modDatabase.opendb(usrdir)
+    modDatabase.getKaratio(usrdir)
+    karatio = modDatabase.openKaratio(usrdir)
+    libra = modDatabase.opendb(usrdir)
     rawdb = libra.get('raw',{})
     keydb = libra.get('key',{})
 
     dtempo = dicto.get('dtempo','')
     utempo = dicto.get('utempo','')
+    takas = dicto.get('takas','')
+    kekad = dicto.get('kekas','')
+    keywo = dicto.get('keywo','')
 
     tiset = modDatabase.timra(usrdir, dtempo=dtempo, utempo=utempo)
 
     modee = True
 
-    resut = []
+    stagun = []
     for rekod in tiset:
         sorse = rawdb.get(rekod,{})
         for keywo in list(sorse.keys()):
@@ -22,6 +28,15 @@ def sumer(usrdir,dicto):
                 if sorse.get(keywo,'') != dicto.get(keywo,''):
                     modee = False
         if modee:
-            resut.append(rekod)
+            stagun.append(rekod)
         modee = True
-    return resut
+
+    stados = {}
+    for rekod in stagun:
+        sorse = rawdb.get(rekod,{})
+        if sorse.get(kekas,'') == keywo:
+            taket = sorse.get(takas,'')
+            upvalu = stados.get(taket,0.0)
+            stados.update({ taket : upvalu })
+
+    return {'stagun':staguno, 'stados':stados}
