94f38e9a5cc1066c4993e95225da1155c8c70c2a
David Soh <sotong0221@gmail.com>
2018-01-31 22:36:31 +0800

    remove listKeywo

--- modDatabase.py
+++ modDatabase.py
@@ -336,22 +336,3 @@ def timra(usrdir, btempo='',ftempo='', modde='uuid'):
         return tuk
     elif modde == 'uuid':
         return datui
-
-
-def listKeywo(usrdir):
-    print('modDatabase.listKeywo: '+usrdir)
-    keydb = opendb(usrdir).get('key',{})
-    resut = {}
-    for kas in keydb.keys():
-        if kas not in ['datte','price','tpric','desci','karen','tkare']:
-            for keywo in keydb.get(kas,{}).keys():
-                numon = 0
-                metdi = {}
-                metse = []
-
-                metdi = resut.get(keywo,{})
-                metse = metdi.get(kas,[])
-                metse.extend(keydb.get(kas,{}).get(keywo,[]))
-                metdi.update({ kas : metse })
-                resut.update({ keywo : metdi })
-    return resut
