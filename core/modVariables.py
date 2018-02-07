5e0ea095dadcb472bcb666c8a122292ef362357a
David Soh <sotong0221@gmail.com>
2018-02-07 18:18:09 +0800

    add tamta (arrangement of temra)

--- core/modVariables.py
+++ core/modVariables.py
@@ -100,6 +100,21 @@ class Argo:
         "toooo":"", "tpric":"", "tkare":"",
         "desci":"",
         }
+
+        self.tamta = {
+            0 : 'datte',
+            1 : 'namma',
+            2 : 'klass',
+            3 : 'shoop',
+            11 : 'fromm',
+            12 : 'karen',
+            13 : 'price',
+            21 : 'toooo',
+            22 : 'tkare',
+            23 : 'tpric',
+            30 : 'desci'
+        }
+
         self.database = {
             'mode' : { 0 : '' },
             'chat' : {
